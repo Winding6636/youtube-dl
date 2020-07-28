5a51b41b8f88a7a3b80fe40152f591576ef65801
Winding <10997628+Winding6636@users.noreply.github.com>
2020-07-29 01:26:11 +0900

    patch fix

--- devscripts/ytdl_pip-patch.sh
+++ devscripts/ytdl_pip-patch.sh
@@ -11,6 +11,7 @@ wget -q -O ./niconico_sm.patch https://raw.githubusercontent.com/Winding6636/you
 #Patched
 _ytdlpath_=`pip show youtube-dl | grep Location | awk '{print $2}'`
 patch -p1 -d $_ytdlpath_ < ./niconico_sm.patch
+sed -i -e '$a __version__ = __version__ + " _nicosm-patch"\n' $_ytdlpath_/youtube_dl/version.py
 #pip metadata update 多分要らない
 _version_=`youtube-dl --version`
 sed -i -e 's/"version": ".*"/"version": "'$_version_'"/g' ''$_ytdlpath_'/youtube_dl-2020.6.16.1.dist-info/metadata.json'
